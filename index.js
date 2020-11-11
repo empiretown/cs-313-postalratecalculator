ee0c9a55262bf510bd291ad02f5cfa7075603cd8
gracekalu <empiretown2016@gmail.com>
2020-11-10 21:30:44 -0700

    all update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/home'))
   .get('/postaldisplay', function (req, res) {
     var requestURL = url.parse(req.url, true);
 
